f1bf83b5138aab12002ec1f4d8c8dbab04661dcd
max027 <saurabhwajage887@gmail.com>
2025-01-30 22:16:15 +0530

    updated headers

--- Server/Server.go
+++ Server/Server.go
@@ -8,18 +8,30 @@ import (
 )
 
 type Server struct {
-	Host string
-	Port string
+	Host        string
+	Port        string
+	status_code map[int]string
+	headers     map[string]string
 }
 
 func (ser *Server) Start() {
+	// move to seprate file
+	ser.status_code = map[int]string{
+		200: "OK",
+		404: "Not Found",
+	}
+	ser.headers = map[string]string{
+		"Server":       "CrudeServer",
+		"Content-Type": "text/html",
+	}
+
 	listner, err := net.Listen(ser.Host, ser.Port)
 	if err != nil {
 		fmt.Println("Error occure while starting server:", err)
 		return
 	}
 	defer listner.Close()
-	fmt.Println("Server is listening on port 8080...")
+	fmt.Println("Server is listening on port ", ser.Port)
 	for {
 		conn, err := listner.Accept()
 		if err != nil {
@@ -28,6 +40,23 @@ func (ser *Server) Start() {
 		}
 		ser.Handel_request(conn)
 	}
+}
+
+func (ser *Server) response_line(status_code int) []byte {
+	reason := ser.status_code[status_code]
+
+	line := []byte(fmt.Sprintf("HTTP/1.1 %d %s\r\n", status_code, reason))
+	return line
+}
+
+func (ser *Server) response_header() []byte {
+	//extra header
+	header := ""
+	for key, value := range ser.headers {
+		header += fmt.Sprintf("%s: %s\r\n", key, value)
+	}
+
+	return []byte(header)
 }
 
 func (ser *Server) Handel_request(conn net.Conn) {
@@ -40,9 +69,9 @@ func (ser *Server) Handel_request(conn net.Conn) {
 	}
 	fmt.Printf("recieved message:%s", message)
 
-	response_line := []byte("HTTP/1.1 200 OK\r\n")
+	response_line := ser.response_line(200)
 
-	headers := bytes.Join([][]byte{[]byte("Server: Simple Server\r\n"), []byte("Content-Type: text/html\r\n")}, nil)
+	header := ser.response_header()
 
 	blank_line := []byte("\r\n")
 
@@ -52,7 +81,7 @@ func (ser *Server) Handel_request(conn net.Conn) {
 	</body>
 	</html>`)
 
-	_, err = conn.Write(bytes.Join([][]byte{response_line, headers, blank_line, response_body}, nil))
+	_, err = conn.Write(bytes.Join([][]byte{response_line, header, blank_line, response_body}, nil))
 	if err != nil {
 		fmt.Println("Error Writing")
 	}
