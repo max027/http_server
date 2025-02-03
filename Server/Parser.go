08375bafb17ffd320f74d6d361e58e6ed9218d49
max027 <saurabhwajage887@gmail.com>
2025-02-03 20:27:56 +0530

    implemented all http requests

--- Server/Parser.go
+++ Server/Parser.go
@@ -1,18 +1,28 @@
 package server
 
 import (
+	"bufio"
 	"bytes"
+	"fmt"
+	"strconv"
 	"strings"
 )
 
-type Parser struct {
+type parser struct {
 	method       string
 	uri          string
 	http_version string
+	header       map[string]string
+	body         string
 }
 
-func (par *Parser) parse(data string) {
+func (par *parser) parse(reader *bufio.Reader) {
+	data, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error Reading:", err)
+	}
 	lines := strings.Split(data, "\r\n")
+
 	if len(lines) > 0 {
 		request_line := []byte(lines[0])
 
@@ -27,4 +37,38 @@ func (par *Parser) parse(data string) {
 			par.http_version = string(words[2])
 		}
 	}
+	par.header = make(map[string]string)
+	var contentLength int
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error Reading header:", err)
+		}
+		trimmed_line := strings.Trim(line, "\r\n")
+
+		if trimmed_line == "" {
+			break
+		}
+		mp := strings.SplitN(trimmed_line, ":", 2)
+		if len(mp) == 2 {
+			key := strings.TrimSpace(mp[0])
+			value := strings.TrimSpace(mp[1])
+			par.header[key] = value
+			if key == "Content-Length" {
+				contentLength, _ = strconv.Atoi(value)
+			}
+		}
+	}
+	if contentLength > 0 {
+		temp := make([]byte, contentLength)
+		_, err := reader.Read(temp)
+		if err != nil {
+			fmt.Println("Error Reading body:", err)
+			return
+		}
+		par.body = string(temp)
+	} else {
+		par.body = ""
+	}
+	fmt.Println(par.body)
 }
--- Server/Server.go
+++ Server/Server.go
@@ -20,12 +20,17 @@ func (ser *Server) Start() {
 	// move to seprate file
 	ser.status_code = map[int]string{
 		200: "OK",
+		204: "No Content",
 		404: "Not Found",
 		501: "Not Implemented",
+		500: "Internal Server Error",
+		400: "Bad Request",
+		201: "Created",
 	}
 	ser.headers = map[string]string{
-		"Server":       "CrudeServer",
-		"Content-Type": "text/html",
+		"Server":         "CrudeServer",
+		"Content-Type":   "text/html",
+		"Content-Length": "0",
 	}
 
 	listner, err := net.Listen(ser.Host, ser.Port)
@@ -65,38 +70,29 @@ func (ser *Server) response_header() []byte {
 func (ser *Server) Handel_request(conn net.Conn) {
 	defer conn.Close()
 	reader := bufio.NewReader(conn)
-	message, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Println("Error Reading:", err)
-		return
-	}
-	fmt.Printf("recieved message:%s", message)
 
-	parser := &Parser{}
-	parser.parse(message)
+	parser := &parser{}
+	parser.parse(reader)
 
-	var response_body []byte
+	var response []byte
 	if parser.method == "GET" {
-		response_body = ser.handel_GET(parser)
+		response = ser.handel_GET(parser)
+	} else if parser.method == "POST" {
+		response = ser.handel_POST()
+	} else if parser.method == "DELETE" {
+		response = ser.handel_DELETE(parser)
+	} else if parser.method == "PUT" {
+		response = ser.handel_PUT(parser)
+	} else {
+		response = ser.handel_501()
 	}
+	conn.Write(response)
 
-	response_line := ser.response_line(200)
-
-	header := ser.response_header()
-
-	blank_line := []byte("\r\n")
-
-	_, err = conn.Write(bytes.Join([][]byte{response_line, header, blank_line, response_body}, nil))
-	if err != nil {
-		fmt.Println("Error Writing")
-	}
 }
 
-func (ser *Server) handel_GET(par *Parser) []byte {
+func (ser *Server) handel_GET(par *parser) []byte {
 	filename := strings.Trim(par.uri, "/")
-	// problem
-	path, err := os.Stat(filename)
-	fmt.Println("path:", path)
+	_, err := os.Stat(fmt.Sprintf("C:\\Users\\saurabh\\programming\\golang\\http_server\\Server\\%s", filename))
 	if err != nil {
 		response_line := ser.response_line(404)
 		response_header := ser.response_header()
@@ -108,7 +104,7 @@ func (ser *Server) handel_GET(par *Parser) []byte {
 	response_line := ser.response_line(200)
 	response_header := ser.response_header()
 	blank_line := []byte("\r\n")
-	response_body, err := readFileAsBytes(par.uri)
+	response_body, err := readFileAsBytes(fmt.Sprintf("C:\\Users\\saurabh\\programming\\golang\\http_server\\Server\\%s", filename))
 	if err != nil {
 		fmt.Println("error reading file")
 	}
@@ -124,6 +120,17 @@ func readFileAsBytes(path string) ([]byte, error) {
 	return data, nil
 }
 
+func (ser *Server) handel_POST() []byte {
+	var response []byte
+	response_line := ser.response_line(201)
+	response_header := ser.response_header()
+	response_body := []byte("resource created")
+	blank_line := []byte("\r\n")
+	response = bytes.Join([][]byte{response_line, response_header, blank_line, response_body}, nil)
+
+	return response
+}
+
 func (ser *Server) handel_501() []byte {
 	response_line := ser.response_line(501)
 
@@ -133,4 +140,40 @@ func (ser *Server) handel_501() []byte {
 	response_body := []byte("<h1>501 not Implemented</h1>")
 
 	return bytes.Join([][]byte{response_line, response_header, blank_line, response_body}, nil)
+}
+
+func (ser *Server) handel_DELETE(par *parser) []byte {
+	filename := par.uri
+	_, err := os.Stat(fmt.Sprintf("C:\\Users\\saurabh\\programming\\golang\\http_server\\Server\\%s", filename))
+	response_header := ser.response_header()
+	blank_line := []byte("\r\n")
+	if err != nil {
+		fmt.Println("Resource Not found")
+		response_body := []byte("<h1>File not found</h1>")
+		response_line := ser.response_line(404)
+		return bytes.Join([][]byte{response_line, response_header, blank_line, response_body}, nil)
+	}
+	response_line := ser.response_line(204)
+	err2 := os.Remove(fmt.Sprintf("C:\\Users\\saurabh\\programming\\golang\\http_server\\Server\\%s", filename))
+	if err2 != nil {
+		fmt.Println("Failed to delete resource")
+		response_line := ser.response_line(500)
+		response_body := []byte("<h1>Internal Server Error</h1>")
+		return bytes.Join([][]byte{response_line, response_header, blank_line, response_body}, nil)
+	}
+	return bytes.Join([][]byte{response_line, response_header, blank_line}, nil)
+}
+func (ser *Server) handel_PUT(par *parser) []byte {
+	filename := par.uri
+	_, err := os.Stat(fmt.Sprintf("C:\\Users\\saurabh\\programming\\golang\\http_server\\Server\\%s", filename))
+	response_header := ser.response_header()
+	blank_line := []byte("\r\n")
+	if err != nil {
+		fmt.Println("Resource Not found")
+		response_body := []byte("<h1>File not found</h1>")
+		response_line := ser.response_line(404)
+		return bytes.Join([][]byte{response_line, response_header, blank_line, response_body}, nil)
+	}
+	response_line := ser.response_line(201)
+	return bytes.Join([][]byte{response_line, response_header, blank_line}, nil)
 }
